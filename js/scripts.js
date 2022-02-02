31650efc3cee2f518a0ed19f2899adfbe7cad5aa
Deya Card <thedeyacard@gmail.com>
2022-02-01 19:51:13 -0800

    test js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
+
 
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -14,8 +21,9 @@ function wordCounter(text) {
   return wordCount;
 }
 
+
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -28,15 +36,24 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+// UI Logic
 
-function includesRarestLetter(word) {
-  if (word.toLowerCase().includes("q")) {
-    return true;
-  }
-  return false;
+function boldPassage(word, text) {
+  let htmlString = "p";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+     htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+     htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "<p>";
 }
 
-// UI Logic
 
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
@@ -47,5 +64,6 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
